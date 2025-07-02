7e0ff67e95544b8e36b74fd9cd4a6bbc836e9949
Richard Herrera <yrichardz3003@gmail.com>
2025-07-02 17:40:38 -0300

    feat(controller): criar endpoint GET para Tarefa

--- src/main/java/com/richarddev/to_do_list/controller/TarefaController.java
+++ src/main/java/com/richarddev/to_do_list/controller/TarefaController.java
@@ -3,21 +3,36 @@ package com.richarddev.to_do_list.Controller;
 
 
 import com.richarddev.to_do_list.DTO.DadosTarefaCreate;
+import com.richarddev.to_do_list.DTO.DadosTarefaRead;
+import com.richarddev.to_do_list.Enums.PrioridadeTarefa;
+import com.richarddev.to_do_list.Model.Tarefa;
+import com.richarddev.to_do_list.Repository.TarefaRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TarefaController {
 
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
     @PostMapping
     @Transactional
     public void criar (@RequestBody @Valid DadosTarefaCreate dados){
-        System.out.println(dados);
+        tarefaRepository.save(new Tarefa(dados));
+    }
+
+    @GetMapping
+    public Page<DadosTarefaRead> listar(
+            @PageableDefault(size = 10, sort = "prioridade", direction = Sort.Direction.DESC) Pageable paginacao) {
+        return tarefaRepository.findAll(paginacao).map(DadosTarefaRead::new);
     }
 
 }
