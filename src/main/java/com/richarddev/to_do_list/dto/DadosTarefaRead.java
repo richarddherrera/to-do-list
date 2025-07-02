5cffbb36dd7ea9c2fe0a413913a80b4b7c3b72a4
Richard Herrera <yrichardz3003@gmail.com>
2025-07-02 16:49:10 -0300

    feat(dto) criar DadosTarefaRead para leitura dos dados

--- src/main/java/com/richarddev/to_do_list/dto/DadosTarefaRead.java
+++ src/main/java/com/richarddev/to_do_list/dto/DadosTarefaRead.java
@@ -2,29 +2,18 @@ package com.richarddev.to_do_list.DTO;
 
 import com.richarddev.to_do_list.Enums.PrioridadeTarefa;
 import com.richarddev.to_do_list.Enums.StatusTarefa;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 
-public record DadosTarefaCreate(
+public record DadosTarefaRead(
 
-        @NotBlank
+        @NotNull Long id,
         String titulo,
-
-        @NotBlank
         String descricao,
-
-        @NotNull
         StatusTarefa status,
-
-        @NotNull
+        LocalDate data_criacao,
         LocalDate prazo,
-
-        @NotNull
-        PrioridadeTarefa prioridade
-) {
-
-
-
+        PrioridadeTarefa prioridade) {
 }
